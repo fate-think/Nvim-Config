ee96ad03cdbc0d17dbbbb72d5830231995f04261
redblue-fate <mulberry@126.com>
2023-05-20 14:58:44 +0800

    add tutorial and md review plugin

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Nvim-Config
 A series of neovim config files
+
+# Tutorial
+
+## Install necesarry dependencies
+$ pkg install neovim-nightly nodejs-lts lua54
+$ npm install -g yarn
+
+## Clone packer.nvim(a package manager) into local directory
+$ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+## clone configuration files into ~/.config/nvim/ from github
+$ git clone https://github.com/fate-think/Nvim-Config ~/.config/nvim/
+
+start nvim,execute:  
+:PackerSync
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,8 @@ return require("packer").startup(function()
     use { "ellisonleao/gruvbox.nvim" }
     use("shaunsingh/nord.nvim")
 	use("cocopon/iceberg.vim")
-	use {
+    -- 代码高亮
+    use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
@@ -28,9 +29,12 @@ return require("packer").startup(function()
     use("hrsh7th/cmp-vsnip")
     -- lsp美化
     use("onsails/lspkind-nvim")
-    -- 模糊匹配工具
+    -- 模糊匹配工具 即搜索工具
     use("junegunn/fzf")
     use("junegunn/fzf.vim")
+    -- markdown预览插件
+    -- install without yarn or npm
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
     -- 状态栏工具
     use {
 		  'nvim-lualine/lualine.nvim',
